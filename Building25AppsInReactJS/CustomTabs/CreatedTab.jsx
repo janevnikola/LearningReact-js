e8bff52cd78362d52d92020d461c5a730ebaf8c1
janevnikola <72938546+janevnikola@users.noreply.github.com>
2024-09-23 16:49:15 +0200

    Create CreatedTab.jsx

---
+++ Building25AppsInReactJS/CustomTabs/CreatedTab.jsx
@@ -0,0 +1,12 @@
+function CreatedTab() {
+  return (
+    <>
+      <div>
+        <h1>Content for tab 4</h1>
+        <p>I created this tab with a separate component</p>
+      </div>
+    </>
+  );
+}
+
+export default CreatedTab;
